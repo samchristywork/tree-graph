f3eeacd818d7567eba2a3bfc2a3b487e1f7da73c
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-03-19 18:44:33 -0500

    feat: Add functions for reading data from stdin
    These functions are very similar, and it might make sense to consolidate
    them into a single function.

--- src/tree.c
+++ src/tree.c
@@ -24,3 +24,51 @@ void print_tree(Tree *tree, int depth) {
     print_tree(tree->children[i], depth + 1);
   }
 }
+
+char **readNodes() {
+  char **lines = NULL;
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  unsigned int count = 0;
+  while ((read = getline(&line, &len, stdin)) != -1) {
+    if (strstr(line, "--\n") == line) {
+      break;
+    }
+    if (line[read - 1] == '\n') {
+      line[read - 1] = '\0';
+    }
+    lines = realloc(lines, sizeof(char *) * (count + 1));
+    lines[count] = strdup(line);
+    count++;
+  }
+  free(line);
+
+  lines = realloc(lines, sizeof(char *) * (count + 1));
+  lines[count] = NULL;
+
+  return lines;
+}
+
+char **readExtra(int *length) {
+  char **lines = NULL;
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  unsigned int count = 0;
+  while ((read = getline(&line, &len, stdin)) != -1) {
+    if (line[read - 1] == '\n') {
+      line[read - 1] = '\0';
+    }
+    lines = realloc(lines, sizeof(char *) * (count + 1));
+    lines[count] = strdup(line);
+    count++;
+  }
+  free(line);
+
+  lines = realloc(lines, sizeof(char *) * (count + 1));
+  lines[count] = NULL;
+
+  *length = count;
+  return lines;
+}
