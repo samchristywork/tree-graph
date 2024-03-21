238c7b4c64eb5516712324eef4fb0fc3477e076d
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-03-20 20:45:35 -0500

    feat: Use the command-line arguments
    These functions are used to get a command-line argument or a default if
    the argument is not set.

--- src/main.c
+++ src/main.c
@@ -13,6 +13,41 @@ int max(int a, int b) {
   return b;
 }
 
+double get_or_double(char c, double def) {
+  if(get_is_set(c)) {
+    char *value = get_value(c);
+    if(strlen(value) > 0) {
+      return atof(value);
+    }
+  }
+
+  return def;
+}
+
+Vec2 get_or_vec2(char c, Vec2 def) {
+  if(get_is_set(c)) {
+    char *value = get_value(c);
+    if(strlen(value) > 0) {
+      char *x = strtok(value, ",");
+      char *y = strtok(NULL, ",");
+      return (Vec2) {atof(x), atof(y)};
+    }
+  }
+
+  return def;
+}
+
+char *get_or_string(char c, char *def) {
+  if(get_is_set(c)) {
+    char *value = get_value(c);
+    if(strlen(value) > 0) {
+      return value;
+    }
+  }
+
+  return def;
+}
+
 int main(int argc, char *argv[]) {
   add_arg('f', "font-size", "Set the size of the font (default 12).");
   add_arg('m', "margin", "Set the margin around the tree (default 10,10).");
@@ -23,14 +58,13 @@ int main(int argc, char *argv[]) {
 
   parse_opts(argc, argv);
 
-  Context ctx= {
-    .font_size = 12,
-    .margin = {10, 10},
-    .pad = {5, 5},
-    .gap = {40, 5},
-    .max_width = 0,
-    .max_height = 0,
-    .filename = "tree.png"
+  Context ctx = {
+    .font_size = get_or_double('f', 12),
+    .margin = get_or_vec2('m', (Vec2) {10, 10}),
+    .pad = get_or_vec2('p', (Vec2) {5, 5}),
+    .gap = get_or_vec2('g', (Vec2) {40, 5}),
+    .infile = get_or_string('i', NULL),
+    .outfile = get_or_string('o', NULL)
   };
 
   char **lines = readNodes();
@@ -63,7 +97,7 @@ int main(int argc, char *argv[]) {
 
   // Write to file
   cairo_surface_t *surface = cairo_get_target(cr);
-  cairo_status_t r = cairo_surface_write_to_png(surface, ctx.filename);
+  cairo_status_t r = cairo_surface_write_to_png(surface, ctx.outfile);
   if (r != CAIRO_STATUS_SUCCESS) {
     printf("Error: could not write to file\n");
     return 1;
